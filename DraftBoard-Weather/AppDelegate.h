10b935093d99828580af43cdbd7321af9f7c657e
Konrad Wright <KonradWright95@Gmail.com>
2018-02-13 11:55:07 -0800

    Initial Commit
    Created blank Xcode project

---
+++ DraftBoard-Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DraftBoard-Weather
+//
+//  Created by Konrad Wright on 2/13/18.
+//  Copyright © 2018 Konrad Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
